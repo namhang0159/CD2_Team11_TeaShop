0bcc115aa39dbe77f4e85d27c977c0853ff3aba8
namhang0159 <namhang0159@gmail.com>
2026-03-29 15:54:03 +0700

    customers

--- src/app/customers/page.jsx
+++ src/app/customers/page.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
 
-const CustomerPage = () => {
-  return <div>CustomerPage</div>;
-};
+import CustomerTable from "@/components/customers/CustomerTable";
+import Link from "next/link";
 
-export default CustomerPage;
+export default function CustomerPage() {
+  return (
+    <div className="p-8 flex flex-col gap-6">
+      <div className="flex flex-wrap items-center justify-between gap-4">
+        <div>
+          <h1 className="text-3xl font-bold">Khách hàng</h1>
+          <p className="text-gray-500 text-sm mt-1">
+            Quản lý thông tin người dùng
+          </p>
+        </div>
+      </div>
+
+      <CustomerTable />
+    </div>
+  );
+}
