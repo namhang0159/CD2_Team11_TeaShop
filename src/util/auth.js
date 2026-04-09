fdb7e5f50460d2ded025c7af78aa77694c2f102d
namhang0159 <namhang0159@gmail.com>
2026-04-09 12:49:47 +0700

    fixxx

--- src/util/auth.js
+++ src/util/auth.js
@@ -3,11 +3,11 @@ import axiosClient from "./axios";
 const Login = async (email, password) => {
   // TODO: Implement login logic
   const data = { email, password };
-  const URL = `${process.env.NEXT_PUBLIC_API_URL}/api/admin/login`;
+  const URL = `/api/admin/login`;
   return axiosClient.post(URL, data);
 };
 const FetchMe = async () => {
-  const URL = `${process.env.NEXT_PUBLIC_API_URL}/api/me`;
+  const URL = `/api/me`;
   return axiosClient.get(URL);
 };
 export { Login, FetchMe };
